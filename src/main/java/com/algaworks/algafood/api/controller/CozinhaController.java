40a693b28a9fe6648331f6135d230f4710f0089a
AlvaroFelipe10 <alvs.felipe@hotmail.com>
2023-03-07 10:31:29 -0300

    implementando a inclusão de restaurantes

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.algaworks.algafood.api.model.CozinhasXmlWrapper;
+
 import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.model.Cozinha;
@@ -40,11 +40,6 @@ public class CozinhaController {
 		return cozinhaRepository.listar();
 	}
 	
-	
-	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
-	public CozinhasXmlWrapper listarXml() {
-		return new CozinhasXmlWrapper(cozinhaRepository.listar());
-	}
 	
 	@GetMapping("/{cozinhaId}")
 	public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
@@ -72,7 +67,7 @@ public class CozinhaController {
 		//cozinhaAtual.setNome(cozinha.getNome());
 		BeanUtils.copyProperties(cozinha, cozinhaAtual, "id"); // o parametro ID, queremos que ignore, para nao dar erro
 		
-		cozinhaRepository.salvar(cozinhaAtual);
+		cozinhaAtual = cadastroCozinha.salvar(cozinhaAtual);
 		return ResponseEntity.ok(cozinhaAtual);
 		}
 		
