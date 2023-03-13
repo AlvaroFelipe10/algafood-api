eafbc6f29caf1ffff4953104e73e7ea9950949e1
AlvaroFelipe10 <alvs.felipe@hotmail.com>
2023-03-13 15:48:43 -0300

    JpaRepository para customizar o repositório base

--- src/main/java/com/algaworks/algafood/api/controller/TesteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/TesteController.java
@@ -1,8 +1,5 @@
 package com.algaworks.algafood.api.controller;
 
-import static com.algaworks.algafood.infrastructure.repository.spec.RestauranteSpecs.comFreteGratis;
-import static com.algaworks.algafood.infrastructure.repository.spec.RestauranteSpecs.comNomeSemelhante;
-
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
@@ -28,56 +25,66 @@ public class TesteController {
 	private RestauranteRepository restauranteRepository;
 	
 	@GetMapping("/cozinhas/por-nome")
-	public List<Cozinha> cozinhasPorNome(String nome){
+	public List<Cozinha> cozinhasPorNome(String nome) {
 		return cozinhaRepository.findTodasByNomeContaining(nome);
 	}
 	
 	@GetMapping("/cozinhas/unica-por-nome")
-	public Optional<Cozinha> cozinhaPorNome(String nome){
+	public Optional<Cozinha> cozinhaPorNome(String nome) {
 		return cozinhaRepository.findByNome(nome);
 	}
 	
 	@GetMapping("/cozinhas/exists")
-	public boolean cozinhaExists(String nome){
+	public boolean cozinhaExists(String nome) {
 		return cozinhaRepository.existsByNome(nome);
 	}
 	
+	@GetMapping("/cozinhas/primeira")
+	public Optional<Cozinha> cozinhaPrimeiro() {
+		return cozinhaRepository.buscarPrimeiro();
+	}
+	
 	@GetMapping("/restaurantes/por-taxa-frete")
-	public List<Restaurante> restaurantePorTaxaFrete(
-			BigDecimal taxaInicial, BigDecimal taxaFinal){
+	public List<Restaurante> restaurantesPorTaxaFrete(
+			BigDecimal taxaInicial, BigDecimal taxaFinal) {
 		return restauranteRepository.queryByTaxaFreteBetween(taxaInicial, taxaFinal);
 	}
 	
 	@GetMapping("/restaurantes/por-nome")
-	public List<Restaurante> restaurantePorTaxaFrete(
-			String nome, long cozinhaId){
+	public List<Restaurante> restaurantesPorTaxaFrete(
+			String nome, Long cozinhaId) {
 		return restauranteRepository.consultarPorNome(nome, cozinhaId);
 	}
 	
 	@GetMapping("/restaurantes/primeiro-por-nome")
-	public Optional<Restaurante> restaurantePrimeiroPorNome(String nome){
+	public Optional<Restaurante> restaurantePrimeiroPorNome(String nome) {
 		return restauranteRepository.findFirstRestauranteByNomeContaining(nome);
 	}
 	
 	@GetMapping("/restaurantes/top2-por-nome")
-	public List<Restaurante> restaurantesTop2PorNome(String nome){
+	public List<Restaurante> restaurantesTop2PorNome(String nome) {
 		return restauranteRepository.findTop2ByNomeContaining(nome);
 	}
 	
-	@GetMapping("/restaurantes/por-nome-frete")
-	public List<Restaurante> restaurantesPorNomeFrete(String nome,
-			BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal){
+	@GetMapping("/restaurantes/por-nome-e-frete")
+	public List<Restaurante> restaurantesPorNomeFrete(String nome, 
+			BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
 		return restauranteRepository.find(nome, taxaFreteInicial, taxaFreteFinal);
 	}
 	
 	@GetMapping("/restaurantes/count-por-cozinha")
-	public int restauranteCountPorCozinha(Long cozinhaId) {
+	public int restaurantesCountPorCozinha(Long cozinhaId) {
 		return restauranteRepository.countByCozinhaId(cozinhaId);
 	}
 	
 	@GetMapping("/restaurantes/com-frete-gratis")
-	public List<Restaurante> restauranteComFreteGratis(String nome){
-		
-		return restauranteRepository.findAll(comFreteGratis().and(comNomeSemelhante(nome)));
+	public List<Restaurante> restaurantesComFreteGratis(String nome) {
+		return restauranteRepository.findComFreteGratis(nome);
 	}
+	
+	@GetMapping("/restaurantes/primeiro")
+	public Optional<Restaurante> restaurantePrimeiro() {
+		return restauranteRepository.buscarPrimeiro();
+	}
+	
 }
--- src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -9,10 +9,6 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
 import javax.persistence.criteria.Predicate;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,10 +20,8 @@ import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
 import com.algaworks.algafood.domain.repository.RestauranteRepositoryQueries;
 
-
-
 @Repository
-public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries{
+public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 
 	@PersistenceContext
 	private EntityManager manager;
@@ -36,36 +30,37 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries{
 	private RestauranteRepository restauranteRepository;
 	
 	@Override
-	public List<Restaurante> find (String nome, 
-			BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal){
+	public List<Restaurante> find(String nome, 
+			BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
 		var builder = manager.getCriteriaBuilder();
 		
 		var criteria = builder.createQuery(Restaurante.class);
-		var root = criteria.from(Restaurante.class); // from restaurante
-		
+		var root = criteria.from(Restaurante.class);
+
 		var predicates = new ArrayList<Predicate>();
 		
-		if(StringUtils.hasText(nome)) {
-				predicates.add(builder.like(root.get("nome"), "%" + nome + "%"));
+		if (StringUtils.hasText(nome)) {
+			predicates.add(builder.like(root.get("nome"), "%" + nome + "%"));
 		}
 		
-		if(taxaFreteInicial != null) {
-		predicates.add(builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial)); 
+		if (taxaFreteInicial != null) {
+			predicates.add(builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial));
 		}
 		
-		if(taxaFreteFinal != null) {
-		predicates.add(builder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal));
+		if (taxaFreteFinal != null) {
+			predicates.add(builder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal));
 		}
 		
 		criteria.where(predicates.toArray(new Predicate[0]));
-				
+		
 		var query = manager.createQuery(criteria);
 		return query.getResultList();
-		
 	}
 
 	@Override
 	public List<Restaurante> findComFreteGratis(String nome) {
-		return restauranteRepository.findComFreteGratis(nome);
+		return restauranteRepository.findAll(comFreteGratis()
+				.and(comNomeSemelhante(nome)));
 	}
+	
 }
