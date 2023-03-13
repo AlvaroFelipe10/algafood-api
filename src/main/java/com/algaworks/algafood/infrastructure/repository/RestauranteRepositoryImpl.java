0a6893dc4301ba80daa93a51ae30919e332ca055
AlvaroFelipe10 <alvs.felipe@hotmail.com>
2023-03-13 09:14:38 -0300

    Adicionando Restrições na cláusula where

--- src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -8,6 +8,9 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import javax.persistence.criteria.Predicate;
+
 
 import org.springframework.stereotype.Repository;
 
@@ -26,8 +29,15 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries{
 		CriteriaBuilder builder = manager.getCriteriaBuilder();
 		
 		CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
-		criteria.from(Restaurante.class); // from Restaurante
+		Root<Restaurante> root = criteria.from(Restaurante.class); // from restaurante
 		
+	
+		Predicate nomePredicate = builder.like(root.get("nome"), "%" + nome + "%" );
+		
+		Predicate taxaInicialPredicate = builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial); 
+		
+		Predicate taxaFinalPredicate = builder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal);
+				
 		TypedQuery<Restaurante> query = manager.createQuery(criteria);
 		return query.getResultList();
 		
