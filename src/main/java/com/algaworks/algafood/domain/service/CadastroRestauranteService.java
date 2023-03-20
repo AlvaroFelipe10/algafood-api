0383012d36eb7dba8dfb9730c65c5c8de5069c35
AlvaroFelipe10 <alvs.felipe@hotmail.com>
2023-03-20 11:35:13 -0300

    Refatorando os serviços REST

--- src/main/java/com/algaworks/algafood/domain/service/CadastroRestauranteService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroRestauranteService.java
@@ -18,6 +18,9 @@ public class CadastroRestauranteService {
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
 	
+	private static final String MSG_RESTAURANTE_NAO_ENCONTRADO 
+    = "Não existe um cadastro de restaurante com código %d";
+	
 	public Restaurante salvar(Restaurante restaurante) {
 		Long cozinhaId = restaurante.getCozinha().getId();
 		Cozinha cozinha = cozinhaRepository.findById(cozinhaId)
@@ -28,4 +31,9 @@ public class CadastroRestauranteService {
 		return restauranteRepository.save(restaurante);
 		}
 		
+	public Restaurante buscarOuFalhar(Long restauranteId) {
+	    return restauranteRepository.findById(restauranteId)
+	        .orElseThrow(() -> new EntidadeNaoEncontradaException(
+	                String.format(MSG_RESTAURANTE_NAO_ENCONTRADO, restauranteId)));
+	}
 	}
